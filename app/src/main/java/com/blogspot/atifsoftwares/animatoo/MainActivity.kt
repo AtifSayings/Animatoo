79e6de3735216e5bacb5521cba31a271f7c326ba
Atif Pervaiz <mohammadatif50@gmail.com>
2021-09-05 16:00:48 +0500

    v1.0.2

--- app/src/main/java/com/blogspot/atifsoftwares/animatoo/MainActivity.kt
+++ app/src/main/java/com/blogspot/atifsoftwares/animatoo/MainActivity.kt
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         binding.animateCard.setOnClickListener {
             startActivity(Intent(this, SecondActivity::class.java))
