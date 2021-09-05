659dbc8c4c8d340a6da9319d934384b6f44715ea
Atif Pervaiz <mohammadatif50@gmail.com>
2021-09-05 12:48:48 +0500

    Changing permission of gradlew

--- gradlew.bat
+++ gradlew.bat
@@ -8,6 +8,8 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
+chmod +x gradlew
+
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
